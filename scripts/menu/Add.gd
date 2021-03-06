3c4086a70f9e54055128d73c0d68825662866791
Brendan Lewis <brendanelewis@outlook.com>
2021-03-06 00:24:32 -0500

    Add drag and drop infobox

--- scripts/menu/Add.gd
+++ scripts/menu/Add.gd
@@ -1,14 +1,10 @@
 extends Button
 
 
-func _ready() -> void:
-	if OS.get_name() == "HTML5":
-		self.disabled = true
-	else:
-		self.disabled = false
-
 func _on_pressed() -> void:
-	if get_parent().get_parent().get_node("TabContainer/Entities").visible:
+	if OS.get_name() == "HTML5":
+		get_parent().get_parent().get_parent().get_node("DragAndDrop").popup_centered()
+	elif get_parent().get_parent().get_node("TabContainer/Entities").visible:
 		get_parent().get_parent().get_parent().get_node("EntityFileSelect").popup_centered()
 	elif get_parent().get_parent().get_node("TabContainer/Textures").visible:
 		get_parent().get_parent().get_parent().get_node("TextureFileSelect").popup_centered()
