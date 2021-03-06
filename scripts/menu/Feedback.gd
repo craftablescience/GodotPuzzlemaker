d7cc96b8d849e8e4527f38cff5f8d64bf877d217
Brendan Lewis <brendanelewis@outlook.com>
2021-03-05 22:00:50 -0500

    Remove warnings

--- scripts/menu/Feedback.gd
+++ scripts/menu/Feedback.gd
@@ -16,8 +16,10 @@ func _ready() -> void:
 func _on_item_pressed(id : int) -> void:
 	match id:
 		0:
+			# warning-ignore:return_value_discarded
 			OS.shell_open("https://github.com/craftablescience/GodotPuzzlemaker/issues/new?assignees=craftablescience&labels=bug&template=bug-report.md&title=%5BBUG%5D+Name+of+issue+here")
 		1:
+			# warning-ignore:return_value_discarded
 			OS.shell_open("https://github.com/craftablescience/GodotPuzzlemaker/issues/new?assignees=craftablescience&labels=enhancement&template=feature-request.md&title=%5BFEATURE%5D+Name+of+feature+idea+here")
 		_:
 			assert(false, "Feedback says how?")
